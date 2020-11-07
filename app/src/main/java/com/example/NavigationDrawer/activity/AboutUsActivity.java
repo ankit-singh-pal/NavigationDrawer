0809eaacfc7e68b767b585af9a1527aea7346a0a
ankit-singh-pal <ankit143sept@gmail.com>
2020-11-07 18:24:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/NavigationDrawer/activity/AboutUsActivity.java
@@ -0,0 +1,39 @@
+package com.example.NavigationDrawer.activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.adonasoft.R;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            // finish the activity
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
